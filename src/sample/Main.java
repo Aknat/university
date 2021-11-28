450488816770dbb4e3dd1c107c8cbe3422122959
Aknat <t.tiuova@gmail.com>
2021-11-28 23:47:34 +0200

    implemented GroupFileStorage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,65 +1,94 @@
 package sample;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+
+import static sample.GroupFileStorage.findFileByGroupName;
+import static sample.GroupFileStorage.saveGroupToCSV;
 
 public class Main {
     public static void main(String[] args) {
 
-        Student student1 = new Student("Leia", "Organa", GenderType.FEMALE, "science", 123455);
-        Student student2 = new Student("Han", "Solo", GenderType.MALE, "science", 123456);
-        Student student3 = new Student("Ben", "Solo", GenderType.MALE, "science", 123457);
-        Student student4 = new Student("Padme", "Amidala", GenderType.FEMALE, "science", 123458);
-        Student student5 = new Student("Boba", "Fett", GenderType.NEUTRAL, "science", 123459);
+//        Student student1 = new Student("Leia", "Organa", GenderType.FEMALE, "science", 123455);
+//        Student student2 = new Student("Han", "Solo", GenderType.MALE, "science", 123456);
+//        Student student3 = new Student("Ben", "Solo", GenderType.MALE, "science", 123457);
+//        Student student4 = new Student("Padme", "Amidala", GenderType.FEMALE, "science", 123458);
+//        Student student5 = new Student("Boba", "Fett", GenderType.NEUTRAL, "science", 123459);
+//
+//        Group groupA = new Group("mars");
+//
+//        try {
+//            groupA.addStudent(student1);
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage(student1));
+//        }
+//        try {
+//            groupA.addStudent(student2);
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage(student2));
+//        }
+//        try {
+//            groupA.addStudent(student3);
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage(student3));
+//        }
+//        try {
+//            groupA.addStudent(student4);
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage(student4));
+//        }
+//        try {
+//            groupA.addStudent(student5);
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage(student5));
+//        }
+//        System.out.println("group after creation:" + "\n" + groupA);
+//
+//        saveGroupToCSV(groupA);
+//
+//        System.out.println(student1.toCSVString());
+//
+//        Student studentX = new Student().fromCSVString("Eno;Cordova; ; trolol;  bye  ; trololo ;");
+//        System.out.println(studentX);
+////
+////
+//        Group groupC = new Group("mercury");
+//        try {
+//            groupC.addStudent(new Student().fromCSVString("Darth;Vader"));
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        try {
+//            groupC.addStudent(studentX);
+//        } catch (FullGroupException e) {
+//            System.out.println(e.getMessage());
+//        }
 
-        Group groupA = new Group("mars");
+//        System.out.println(groupC);
+//
+//
 
-        try {
-            groupA.addStudent(student1);
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage(student1));
-        }
-        try {
-            groupA.addStudent(student2);
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage(student2));
-        }
-        try {
-            groupA.addStudent(student3);
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage(student3));
-        }
-        try {
-            groupA.addStudent(student4);
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage(student4));
-        }
-        try {
-            groupA.addStudent(student5);
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage(student5));
-        }
-        System.out.println("group after creation:" + "\n" + groupA);
-
-        System.out.println(student1.toCSVString());
-
-        Student studentX = new Student().fromCSVString("Eno;Cordova; ; ;  bye  ; trololo ;");
-        System.out.println(studentX);
+        File folder = new File("/Users/Jupiter/Documents/Development/education/java/progua/university/src/groupFiles");
 
 
-        Group groupC = new Group("mercury");
-        try {
-            groupC.addStudent(new Student().fromCSVString(""));
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage());
-        }
+//        File [] files = folder.listFiles();
+//        System.out.println(Arrays.toString(files));
 
-        try {
-            groupC.addStudent(studentX);
-        } catch (FullGroupException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println(findFileByGroupName("mars", folder));
 
-        System.out.println(groupC);
+
+
+
+
+
+//        GroupFileStorage.saveGroupToCSV(groupA);
+
+//        File x = new File("/Users/Jupiter/Documents/Development/education/java/progua/university/src/groupFiles/mars.csv");
+//        Group a = new Group(x);
+
+//        System.out.println(a);
 
 
 //        System.out.println();
